0af0e6e933925fe92055515484902236cec4eee0
GustavoGajdeczka <tavinhogustavog@gmail.com>
2020-06-27 20:33:59 -0300

    adicionando o Selenium ao projeto

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IWebDriver driver = new ChromeDriver(@"");
+            driver.Url = "https://www.google.com";
+
+            Console.WriteLine(driver.Title);
+            IWebElement textbox = driver.FindElement(Byte.Name("q"));
+            textbox.SendKeys("Cat Pictures");
+
+            textbox.Submit();
+            Console.WriteLine(driver.Title);
+
+            driver.Quit();
+
+
         }
     }
