af6b1f3918b26339b5f0a1b82189e89ef9858744
GustavoGajdeczka <tavinhogustavog@gmail.com>
2020-06-27 20:15:26 -0300

    iniciando o projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebScrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
